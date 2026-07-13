04ef3e3cb15c1ec7a84bc523dd5e761ee48fdf6a
Ben Gray <bendoylegray@gmail.com>
2026-07-13 17:27:57 -0500

    So many visual changes and code changes

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -2,7 +2,8 @@ import { isMobileDevice } from "./utils.js";
 
 // Scroll to the top of the page when it's loaded. This overrides default browser behavior
 window.onbeforeunload = function () {
-  window.scrollTo(0, 0);
+  // window.scrollTo(0, 0);
+  console.log("Do I need to scroll to top?");
 };
 
 // When the user scrolls down 50px from the top of the document, resize the logo and move it.
